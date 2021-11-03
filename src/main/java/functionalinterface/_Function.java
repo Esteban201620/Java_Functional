a77a472993863b6dc2018882ccbeefa5efee8895
MEOR <estebiosorio@gmail.com>
2021-11-03 12:59:44 -0500

    fifth commit. In this, i added a new java class '_Function', where write a functional function to increment a numbre by one

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -1,4 +1,20 @@
 package functionalinterface;
 
+import java.util.function.Function;
+
 public class _Function {
+    public static void main(String[] args) {
+        int increment = incrementByOne(0);
+        System.out.println(increment);
+
+        int increment2 = incrementByOneFunction.apply(0);
+        System.out.println(increment2);
+    }
+
+    static Function<Integer, Integer> incrementByOneFunction =
+            number -> number ++;
+
+    static int incrementByOne(int number){
+        return number +1;
+    }
 }
